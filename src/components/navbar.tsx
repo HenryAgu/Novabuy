89cf602b6e62512fa24507b3e57555791f88feb8
Henry Agu <aguhenrychuks@gmail.com>
2025-09-18 10:37:27 +0100

    feat: mobile nav bar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -4,6 +4,9 @@ import React from "react";
 import ShoppingBag from "./icons/shoppingBag";
 import User from "./icons/user";
 import SearchIcon from "./icons/searchIcon";
+import MenuIcon from "./icons/menu-icon";
+import { Sheet, SheetClose, SheetContent, SheetTrigger } from "./ui/sheet";
+import CloseIcon from "./icons/close-icon";
 
 interface NavMenu {
   name?: string;
@@ -35,34 +38,53 @@ const navMenu: NavMenu[] = [
 
 const Navbar = () => {
   return (
-    <nav className="flex items-center justify-between py-6 px-5 lg:px-24">
+    <nav className="flex items-center justify-between py-5 lg:py-6 px-5 lg:px-24">
       <Image
         src="/icons/Logo.svg"
         alt="Logo"
         width={177}
         height={42}
-        className="aspect-[177/42]"
+        className="w-[120px] h-auto lg:w-[177px] aspect-[177/42]"
       />
-      <ul className="flex items-center gap-12">
+
+      <ul className="hidden lg:flex items-center gap-12">
         {navMenu.map((item) => (
           <li key={item.name}>
             <Link href={item.link}>{item.name}</Link>
           </li>
         ))}
       </ul>
-      <div className="flex items-center gap-10">
+      <div className="flex items-center gap-5 lg:gap-10">
         <button>
           <ShoppingBag />
         </button>
         <button>
           <User />
         </button>
-        <button>
+        <button className="hidden lg:flex">
           <SearchIcon />
         </button>
+        <Sheet>
+          <SheetTrigger className="lg:hidden">
+            <MenuIcon />
+          </SheetTrigger>
+          <SheetContent side="top" className="w-full min-h-40">
+            <MobileMenu />
+          </SheetContent>
+        </Sheet>
       </div>
     </nav>
   );
 };
 
 export default Navbar;
+
+const MobileMenu = () => {
+  return (
+    <section className="p-5 flex flex-col gap-y-6">
+      <SheetClose>
+        <CloseIcon />
+      </SheetClose>
+    </section>
+  );
+};
