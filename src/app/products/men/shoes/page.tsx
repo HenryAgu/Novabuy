507e103340a30f77340882df3e31888da8822331
Henry Agu <aguhenrychuks@gmail.com>
2025-09-21 21:43:33 +0100

    feat: load more

--- src/app/products/men/shoes/page.tsx
+++ src/app/products/men/shoes/page.tsx
@@ -1,7 +1,9 @@
+"use client";
 import ProductCardItem from "@/components/product-card-item";
 import { ProductCard } from "@/components/types";
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
 import { Rubik } from "next/font/google";
-import React from "react";
+import React, { useState } from "react";
 
 const rubik = Rubik({
   variable: "--font-rubik",
@@ -84,6 +86,11 @@ const swiperProducts: ProductCard[] = [
 ];
 
 const ShoePage = () => {
+  const [renderedShoes, setRenderedShoes] = useState<number>(4);
+
+  const renderMoreShoes = () => {
+    setRenderedShoes((prev) => prev + 4);
+  };
   return (
     <main className="lg:px-24 lg:py-8 px-5 py-8 ">
       <div className="flex items-center justify-between">
@@ -91,23 +98,33 @@ const ShoePage = () => {
           Shoes
           <span className={`${rubik.className} text-base font-normal`}>
             {" "}
-            (52 Products)
+            ({swiperProducts.length} Products)
           </span>
         </span>
         <div className="flex items-center gap-x-5.5">
-          <button>Filter</button>
+          <Sheet>
+            <SheetTrigger>Filter</SheetTrigger>
+            <SheetContent>Test</SheetContent>
+          </Sheet>
           <span>|</span>
           <button>Sort by</button>
         </div>
       </div>
       <div className="grid grid-cols-2 lg:grid-cols-4 gap-x-4 gap-y-6 lg:gap-y-12 my-5 lg:my-10">
-        {swiperProducts.map((item, index) => (
+        {swiperProducts.slice(0, renderedShoes).map((item, index) => (
           <ProductCardItem item={item} key={index} />
         ))}
       </div>
-      <div className="flex justify-center">
-        <button className="text-base text-white font-normal bg-primary-500 py-2.5 px-5 rounded-[50px]">Load More</button>
-      </div>
+      {renderedShoes < swiperProducts.length && (
+        <div className="flex justify-center">
+          <button
+            className="text-base text-white font-normal bg-primary-500 py-2.5 px-5 rounded-[50px] cursor-pointer"
+            onClick={renderMoreShoes}
+          >
+            Load More
+          </button>
+        </div>
+      )}
     </main>
   );
 };
