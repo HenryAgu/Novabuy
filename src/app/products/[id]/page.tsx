9f24f9f5a11eb9d16c37e4e3f281c40b7a112431
Henry Agu <aguhenrychuks@gmail.com>
2025-09-23 15:16:02 +0100

    fix: service worker removed

--- src/app/products/[id]/page.tsx
+++ src/app/products/[id]/page.tsx
@@ -4,7 +4,7 @@ const ProductPage = async ({ params }: { params: Promise<{ id: string }> }) => {
   const { id } = await params;
 
   return (
-    <main className="min-h-screen flex gap-x-7">
+    <main className="min-h-screen flex gap-x-18 pr-5 lg:pr-24">
       <div className="basis-[50%] bg-black">1</div>
       <div className="basis-[50%] bg-black">Produgct ID: {id}</div>
     </main>
