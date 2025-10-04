44d6e7b6944c3cacaf49f4891a6fff8bb0a97dcd
Henry Agu <aguhenrychuks@gmail.com>
2025-10-04 09:59:05 +0100

    testing tailwindcss

--- src/app/auth/layout.tsx
+++ src/app/auth/layout.tsx
@@ -2,20 +2,20 @@ import Image from "next/image";
 import React, { ReactNode } from "react";
 
 interface AuthLayoutProps {
-  children: ReactNode;
+  children: ReactNode; 
 }
 
 const AuthLayout: React.FC<AuthLayoutProps> = ({ children }) => {
   return (
     <main className="flex lg:flex-row flex-col">
-      <div className="basis-[100%] lg:basis-[50%]">{children}</div>
+      <div className="basis-[100%] lg:basis-[50%] min-h-screen">{children}</div>
       <div className="hidden lg:flex basis-[50%]">
         <Image
           src="/images/login.png"
           alt="login-page"
           width={760}
           height={1040}
-          className="w-full h-full"
+          className="w-full h-full object-cover"
         />
       </div>
     </main>
--- src/app/auth/sign-up/page.tsx
+++ src/app/auth/sign-up/page.tsx
@@ -1,11 +1,46 @@
-import React from 'react'
+import { Rubik } from "next/font/google";
+import Image from "next/image";
+import React from "react";
+
+const rubik = Rubik({
+  variable: "--font-rubik",
+  subsets: ["latin"],
+});
 
 const SignUpPage = () => {
   return (
-    <div>
-      Sign Up Page
+    <div
+      className="p-20 bg-red-900 min-h-screen"
+      style={{
+        paddingTop: "40px",
+        paddingBottom: "40px",
+        paddingLeft: "80px",
+        paddingRight: "80px",
+      }}
+    >
+      <Image
+        src="/icons/Logo2.svg"
+        alt="logo"
+        width={42}
+        height={42}
+        className="aspect-square"
+      />
+      <div
+        className="flex flex-col gap-y-2 my-10"
+        style={{ maxWidth: "393px" }}
+      >
+        <p className="text-4xl font-bold text-neutral-600 leading-[150%]">
+          Welcome Back!
+        </p>
+        <p
+          className={`${rubik.className} text-base font-normal text-neutral-400 leading-[150%]`}
+        >
+          Please enter your credentials to access your account and explore our
+          latest products.
+        </p>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default SignUpPage
+export default SignUpPage;
