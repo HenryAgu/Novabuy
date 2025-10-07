3e36cbea18d8c5dc858f891e29867284a683366d
Henry Agu <aguhenrychuks@gmail.com>
2025-10-07 11:17:21 +0100

    fix: data type issue

--- src/app/products/men/page.tsx
+++ src/app/products/men/page.tsx
@@ -2,16 +2,15 @@ import SwiperProducts from "@/components/home/swiper-products";
 import CarouselSectionProducts from "@/components/home/carousel-section-products";
 import WhoFor from "@/components/home/who-for";
 import React from "react";
-import { carouselProducts, products, swiperProducts } from "@/components/data/products";
 
 const MenPage = () => {
   return (
     <main className="lg:pb-10 pb-8 ">
       <div className="min-h-screen">
-        <WhoFor title="MEN SHOPPING CATEGORIES" products={products} />
-        <SwiperProducts title="NEW PRODUCTS" products={swiperProducts} />
-        <SwiperProducts title="TRENDING PRODUCTS" products={swiperProducts} />
-        <CarouselSectionProducts title="Explore" products={carouselProducts} />
+        <WhoFor title="MEN SHOPPING CATEGORIES" />
+        <SwiperProducts title="NEW PRODUCTS" />
+        <SwiperProducts title="TRENDING PRODUCTS" />
+        <CarouselSectionProducts title="Explore" />
       </div>
     </main>
   );
