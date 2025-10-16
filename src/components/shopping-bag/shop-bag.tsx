f28f2264bf72f6da23aa5b11c0ba232f9e28788a
Henry Agu <aguhenrychuks@gmail.com>
2025-10-16 09:49:18 +0100

    style: fixes

--- src/components/shopping-bag/shop-bag.tsx
+++ src/components/shopping-bag/shop-bag.tsx
@@ -4,13 +4,14 @@ import CloseIcon from "../icons/close-icon";
 import ShopItems from "./shop-items";
 import SummaryItems from "../check-out/summary-items";
 import Total from "../check-out/total";
+import { ScrollArea } from "../ui/scroll-area";
 
 const ShopBag = () => {
   return (
     <div>
       <SheetContent
         side="top"
-        className="w-full max-w-full h-full lg:w-full lg:max-w-full py-5 lg:py-12 px-5 lg:px-24 mx-auto"
+        className="w-full max-w-full h-full lg:w-full lg:max-w-full py-5  pb-12 lg:py-12 px-5 lg:px-24 mx-auto"
       >
         <SheetTitle className="flex items-center justify-between">
           <div />
@@ -18,15 +19,17 @@ const ShopBag = () => {
             <CloseIcon />
           </SheetClose>
         </SheetTitle>
-        <section className="flex lg:flex-row flex-col gap-y-12 gap-x-24 ">
-          {/* Shopped Items */}
-          <ShopItems />
-          {/* Summary For Checkout */}
-          <div className="basis-[50%] flex flex-col w-full">
-            <SummaryItems />
-            <Total />
-          </div>
-        </section>
+        <ScrollArea className="h-full w-full">
+          <section className="flex lg:flex-row flex-col gap-y-12 gap-x-24 ">
+            {/* Shopped Items */}
+            <ShopItems />
+            {/* Summary For Checkout */}
+            <div className="basis-[50%] flex flex-col w-full">
+              <SummaryItems />
+              <Total />
+            </div>
+          </section>
+        </ScrollArea>
       </SheetContent>
     </div>
   );
