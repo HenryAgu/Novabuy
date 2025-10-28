5c0946d5af4150aad35c4c6fcc4d2e0b4239679b
Henry Agu <aguhenrychuks@gmail.com>
2025-10-28 14:15:22 +0100

    feat: payment option page

--- src/components/payment/payment-form.tsx
+++ src/components/payment/payment-form.tsx
@@ -5,73 +5,65 @@ import Image from "next/image";
 
 const PaymentForm = () => {
   const publicKey = process.env.NEXT_PUBLIC_PAYSTACK_TEST_KEY;
+
+  const paymentOptions = [
+    {
+      src: "/images/mastercard.webp",
+      type: "mastercard",
+      alt: "master-card",
+      width: 36,
+      height: 28,
+    },
+    {
+      src: "/images/visa.webp",
+      type: "visa",
+      alt: "visa",
+      width: 45,
+      height: 15,
+    },
+    {
+      src: "/images/paypal.webp",
+      type: "paypal",
+      alt: "pay-pal",
+      width: 25,
+      height: 30,
+    },
+    {
+      src: "/images/american-express.webp",
+      type: "american-express",
+      alt: "american-express",
+      width: 30,
+      height: 20,
+    },
+  ];
+
   return (
     <div className="px-5 lg:px-0">
-      <p
-        className={`font-bold text-base lg:text-lg text-neutral-600 leading-[150%]`}
-      >
+      <p className="font-bold text-base lg:text-lg text-neutral-600 leading-[150%]">
         Payment Information
       </p>
+
       <div className="my-5">
-        <p>How would like to pay?</p>
-<div className="flex items-center gap-x-2.5 my-5 flex-wrap">
-  <Button
-    type="button"
-    variant="ghost"
-    className="border border-[#E9E9E9] hover:border-primary-500 hover:bg-[#EFEFFE] w-[70px] h-[40px] lg:w-[106px] lg:h-[57px] cursor-pointer flex items-center justify-center"
-  >
-    <Image
-      width={36}
-      height={28}
-      src="/images/mastercard.webp"
-      alt="master-card"
-      className="aspect-[48/37] object-contain"
-    />
-  </Button>
+        <p>How would you like to pay?</p>
 
-  <Button
-    type="button"
-    variant="ghost"
-    className="border border-[#E9E9E9] hover:border-primary-500 hover:bg-[#EFEFFE] w-[70px] h-[40px] lg:w-[106px] lg:h-[57px] cursor-pointer flex items-center justify-center"
-  >
-    <Image
-      width={45}
-      height={15}
-      src="/images/visa.webp"
-      alt="visa"
-      className="aspect-[63/20] object-contain"
-    />
-  </Button>
-
-  <Button
-    type="button"
-    variant="ghost"
-    className="border border-[#E9E9E9] hover:border-primary-500 hover:bg-[#EFEFFE] w-[70px] h-[40px] lg:w-[106px] lg:h-[57px] cursor-pointer flex items-center justify-center"
-  >
-    <Image
-      width={25}
-      height={30}
-      src="/images/paypal.webp"
-      alt="pay-pal"
-      className="aspect-[31/36] object-contain"
-    />
-  </Button>
-
-  <Button
-    type="button"
-    variant="ghost"
-    className="border border-[#E9E9E9] hover:border-primary-500 hover:bg-[#EFEFFE] w-[70px] h-[40px] lg:w-[106px] lg:h-[57px] cursor-pointer flex items-center justify-center"
-  >
-    <Image
-      width={30}
-      height={20}
-      src="/images/american-express.webp"
-      alt="american-express"
-      className="aspect-[44/29] object-contain"
-    />
-  </Button>
-</div>
-
+        <div className="flex items-center gap-x-2.5 my-5 flex-wrap">
+          {paymentOptions.map((option, index) => (
+            <Button
+              key={index}
+              type="button"
+              variant="ghost"
+              className="border border-[#E9E9E9] hover:border-primary-500 hover:bg-[#EFEFFE] w-[70px] h-[40px] lg:w-[106px] lg:h-[57px] cursor-pointer flex items-center justify-center"
+            >
+              <Image
+                src={option.src}
+                alt={option.alt}
+                width={option.width}
+                height={option.height}
+                className="object-contain"
+              />
+            </Button>
+          ))}
+        </div>
       </div>
     </div>
   );
